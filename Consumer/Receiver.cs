3a5c75e68c4f3b0d1a8a24978b64d55620699b45
Strauberg <StefanStrauberg@gmail.com>
2024-05-08 10:18:22 +0300

    finish with receiver app

--- Consumer/Receiver.cs
+++ Consumer/Receiver.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 Console.WriteLine(new String('-', 5) + "RabbitMQ Receiver App" + new String('-', 5));
 
@@ -9,11 +10,15 @@ using var channel = connection.CreateModel();
 
 channel.QueueDeclare(queue: "BasicTest", durable: false, autoDelete: false, exclusive: false, arguments: null);
 
-string message = "Getting started with .NET Core RabbitMQ";
-var body = Encoding.UTF8.GetBytes(message);
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+  var body = ea.Body.ToArray();
+  var message = Encoding.UTF8.GetString(body);
+  Console.WriteLine("Received message: {0}", message);
+};
 
-channel.BasicPublish(exchange: "", routingKey: "BasicTest", basicProperties: null, body: body);
+channel.BasicConsume(queue: "BasicTest", autoAck: true, consumer: consumer);
 
-Console.WriteLine("Sent message: {0}", message);
 Console.WriteLine("Press [Enter] to exit Sender App.");
 Console.ReadLine();
