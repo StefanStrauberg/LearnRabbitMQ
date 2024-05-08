421db16e7317e16f84c440c5219ab279a043c53a
Strauberg <StefanStrauberg@gmail.com>
2024-05-08 11:26:44 +0300

    Implement Interfaces and classes for Domain.Core

---
+++ src/Domain/MicroRabbit.Domain.Core/Events/Event.cs
@@ -0,0 +1,8 @@
+namespace MicroRabbit.Domain.Core.Events;
+
+public abstract class Event
+{
+  public DateTime TimeStamp { get; protected set; }
+  protected Event()
+    => TimeStamp = DateTime.Now;
+}
